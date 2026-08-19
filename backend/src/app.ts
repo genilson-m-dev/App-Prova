a8d1ad3fc9e221fe00b183f693469fefdf407d88
genilson-m-dev <genilson.monteiro.dev@gmail.com>
2026-08-19 20:25:28 -0300

    Feat: Add as primeiras partes dos scripts: app.ts e do server.ts, ambas estao rodando correto e ja consegue abrir no navegador com a rota: http://localhost:9999/health

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,19 @@
 // app.ts que  → configura a aplicação.
+
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app = express(); // Cria uma instância do Express
+
+app.use(helmet()); // Adiciona o middleware Helmet para segurança
+app.use(cors()); // Adiciona o middleware CORS para permitir requisições de diferentes origens
+app.use(express.json()); // Adiciona o middleware para parsear o corpo das requisições como JSON
+
+app.get("/health", (_req, res) => {
+    res.status(200).json({ status: "UP",
+    message: "A aplicação está funcionando corretamente."
+   }); // Rota de verificação de saúde da aplicação
+});
+
+export default app; // Exporta a instância do Express para ser usada em outros arquivos
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,19 @@
+// server.ts → inicia o servidor HTTP.
+//server.ts
+ //  │
+//   │ inicia
+//   ▼
+// app.ts
+//   │
+//   ├── Express
+//   ├── Middlewares
+//   ├── Rotas
+//   └── Configurações
 console.log("Hello, world!");
+import app from "./App"; // Importa a instância do Express configurada no arquivo App.ts
+
+const PORT = process.env.PORT || 9999; // Define a porta do servidor, usando a variável de ambiente PORT ou 3000 como padrão
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
