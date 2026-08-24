c6ef42a32e3059ea247311d0783d667c886d369b
genilson-m-dev <genilson.monteiro.dev@gmail.com>
2026-08-23 22:30:43 -0300

    Feat: Adicionei o themeRouter ficticio para testes de rotas

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -3,7 +3,10 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
 const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL!,
+
+
+// o ! é uma afirmação para o TypeScript de que essa variável estará definida em tempo de execução.
 });
 
 export const prismaDB = new PrismaClient({ adapter });
